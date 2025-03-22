ddcf88aff8a498732936460c2e592951fb0d3b27
rishisulakhe <rishiprasadsulakhe@gmail.com>
2025-03-22 17:50:39 +0000

    Next-auth

---
+++ apps/user-app/app/lib/auth.ts
@@ -0,0 +1,68 @@
+import db from "@repo/db"
+import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from "bcrypt"
+export const authOptions = {
+    providers: [
+      CredentialsProvider({
+          name: 'Credentials',
+          credentials: {
+            phone: { label: "Phone number", type: "text", placeholder: "1231231231", required: true },
+            password: { label: "Password", type: "password", required: true }
+          },
+          // TODO: User credentials type from next-aut
+          async authorize(credentials: any) {
+            // Do zod validation, OTP validation here
+            if (!credentials?.phone || !credentials?.password) {
+                throw new Error("Phone number and password are required");
+              }
+           
+            const existingUser = await db.user.findFirst({
+                where: {
+                    number: credentials.phone
+                }
+            });
+
+            if (existingUser) {
+                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                if (passwordValidation) {
+                    return {
+                        id: existingUser.id.toString(),
+                        name: existingUser.name,
+                        number: existingUser.number
+                    }
+                }
+                return null;
+            }
+
+            try {
+                const hashedPassword = await bcrypt.hash(credentials.password, 10);
+                const user = await db.user.create({
+                    data: {
+                        number: credentials.phone,
+                        password: hashedPassword
+                    }
+                });
+            
+                return {
+                    id: user.id.toString(),
+                    name: user.name,
+                    email: user.number
+                }
+            } catch (error) {
+                console.error("Signup error:", error);
+                throw new Error("Error creating user");
+              }
+          },
+        })
+    ],
+    secret: process.env.JWT_SECRET || "secret",
+    callbacks: {
+        // TODO: can u fix the type here? Using any is bad
+        async session({ token, session }: any) {
+            session.user.id = token.sub
+
+            return session
+        }
+    }
+  }
+  
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,17 +1,14 @@
+"use client"
+
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-import { PrismaClient } from "@repo/db";
-const client =new PrismaClient();
-
+import { Appbar } from "@repo/ui/appbar";
+import { signIn,signOut , useSession} from "next-auth/react";
 export default function Home() {
+  const sesssion=useSession();
   return (
     <div>
-      <div className="text-red-500">
-        Hi Rishi beta
-      </div>
-      <Button appName=" app">
-        Hi their
-      </Button>
+      <Appbar onSignin={signIn} onSignout={signOut} user={sesssion.data?.user}/>
     </div>)
 }
